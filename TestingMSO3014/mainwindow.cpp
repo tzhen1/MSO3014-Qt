6f3baecc22135a96716cff31865323d39c0b33f9
tzhen1 <79606359+tzhen1@users.noreply.github.com>
2021-08-19 13:47:06 +0100

    Add files via upload
    MSO reset function completed

--- TestingMSO3014/mainwindow.cpp
+++ TestingMSO3014/mainwindow.cpp
@@ -16,27 +16,43 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    ViSession rm= VI_NULL, vi= VI_NULL; //rm = resource manager (for finding resources and opening them)
-    ViStatus status;
-    ViChar buffer[256];
+    bool Mso3014Active= false;
+    ViSession rm= VI_NULL, vi= VI_NULL; //rm=resource manager,vi=visa, MSO (finding resources and opening them)
+    ViStatus Status;
+    ViChar buffer[256]; // holds responses from I/O (write/read)
     ViUInt32 retCnt;
 
     //open default session
-    status = viOpenDefaultRM(&rm);//access to default resource manager
-    if(status< VI_SUCCESS) goto ERROR_CLEANUP; //if not success
+    Status = viOpenDefaultRM(&rm);//access to default resource manager
+    if(Status< VI_SUCCESS) goto ERROR_CLEANUP; //if not success
 
     //open USB Port using Vendor id, model id, serial no
-    status = viOpen(rm, "USB::1689::1057::C010783::INSTR ", VI_NULL,VI_NULL, &vi); //USBTMC config
-    if(status< VI_SUCCESS) goto ERROR_CLEANUP;
+    Status = viOpen(rm, "USB::1689::1057::C010783::INSTR ", VI_NULL,VI_NULL, &vi); //USBTMC config
+    if(Status< VI_SUCCESS) goto ERROR_CLEANUP;
+
+    //Clean and reset
+    Status = viWrite( vi , (ViBuf)"*CLS;*OPC" , 9 , &retCnt );
+    if(Status< VI_SUCCESS) goto ERROR_CLEANUP;
+
+    Status =  viWrite( vi , (ViBuf)"*RST;*OPC" , 9 , &retCnt );
+    if(Status< VI_SUCCESS) goto ERROR_CLEANUP;
+    else{ qDebug() << "Scope INIT"; }
+
+    Mso3014Active= true;
 
     //send *IDN? query
-    status = viWrite( vi , (ViBuf)"*idn?" , 5 , &retCnt ); //identify
-    if(status< VI_SUCCESS) goto ERROR_CLEANUP;
+    Status = viWrite( vi , (ViBuf)"*idn?" , 5 , &retCnt ); //identify
+    if(Status< VI_SUCCESS) goto ERROR_CLEANUP;
+
+//    //Auto setup
+//    Status = viWrite( vi , (ViBuf)"AUTOS EXEC;*OPC?" , 16 , &retCnt ); //identify
+//    if(Status< VI_SUCCESS) goto ERROR_CLEANUP;
+//    else{ qDebug() << "Scope autoset"; }
 
     //clean buffer and read response
     memset(buffer,0, sizeof(buffer));
-    status = viRead( vi, (ViBuf)buffer , sizeof(buffer) , &retCnt );
-    if(status< VI_SUCCESS) goto ERROR_CLEANUP;
+    Status = viRead( vi, (ViBuf)buffer , sizeof(buffer) , &retCnt );
+    if(Status< VI_SUCCESS) goto ERROR_CLEANUP;
 
     //print response
     qDebug() << "id: " <<buffer;
@@ -47,8 +63,8 @@ void MainWindow::on_pushButton_clicked()
 
 ERROR_CLEANUP:
 
-    viStatusDesc(vi,status,buffer);
-    qDebug() << "Error: " << buffer;
+    viStatusDesc(vi,Status,buffer);
+    qDebug() << "Status: " << buffer;
     if (rm!= VI_NULL)
     {
         viClose(rm);
